35ff041af2ed4c0af93b7f322795513f5c6876d7
chris <cmead87@gmail.com>
2024-02-09 16:45:52 -0800

    please upload my gyad

--- greedy2/greedy2/greedy2.cpp
+++ greedy2/greedy2/greedy2.cpp
@@ -3,13 +3,12 @@
 #include <iomanip>
 #include <fstream>
 #include <vector>
-
+#include <cmath>
 using namespace std;
 
 int start();
 int evaluate(double a, double b, double c);
 double calculate(double next, double counter, double average);
-
 
 
 int main() {
@@ -20,81 +19,84 @@ int main() {
     double blue[10] = { 6.00449072, 3.34005839, 6.71096916, 4.11113061, 5.68416528, 3.88539945, 3.51181469, 3.67426432, 4.98069804, 4.41366311 };
     double red[10] = { 6.36086896, 5.65584783, 7.62912922, 13.29826146, 5.99876216, 8.14484021, 9.74488991, 6.616229, 14.26793535, 0.98932393 };
 
-    double greenav = 0;
-    double countergreen = 1;
-    double blueav = 0;
-    double counterblue = 1;
-    double redav = 0;
-    double counterred = 1;
-    double hold, hold2, hold3;
-
+    double greenav = 13;
+    double countergreen = 0;
+    double blueav = 13;
+    double counterblue = 0;
+    double redav = 13;
+    double counterred = 0;
+    double allav = 0;
+    double counterall = 0;
+    double ucbg, ucbb, ucbr = 0;
 
     vector<double> greenv;
     vector<double> bluev;
     vector<double> redv;
+    vector<double> allv;
+
 
     for (int i = 0; i < 100; i++)
     {
-        int largest = evaluate(greenav, blueav, redav);
+        //int largest = evaluate(greenav, blueav, redav);
         int r10 = rand() % 10; //picks a random number 0-9 length of the array
+        if (countergreen == 0) {
+            countergreen = greenav;
+            ucbg = greenav + (3 * sqrt((log(i)) / countergreen));
+        }
+        else {
+            ucbg = greenav + (3 * sqrt((log(i)) / countergreen));
+        }
+
+        if (counterblue == 0) {
+            counterblue = blueav;
+            ucbb = blueav + (3 * sqrt((log(i)) / counterblue));
+        }
+        else {
+            ucbb = blueav + (3 * sqrt((log(i)) / counterblue));
+        }
+
+        if (counterred == 0) {
+            counterred = redav;
+            ucbr = redav + (3 * sqrt((log(i)) / counterred));
+        }
+        else {
+            ucbr = redav + (3 * sqrt((log(i)) / counterred));
+        }
+        
+        int largest = evaluate(ucbg, ucbb, ucbr);
 
         switch (largest) {
         case 1:
-            cout << i << " current green array pick: " << green[r10] << setw(15);
-            hold = calculate(green[r10], countergreen, greenav);
-            greenav = hold;
             countergreen = countergreen + 1;
-            cout << greenav << " Green Average " << endl;
+            greenav = calculate(green[r10], countergreen, greenav);
+            counterall = counterall + 1;
+            allav = calculate(green[r10], counterall, allav);
             break;
 
         case 2:
-            cout << i << " current blue array pick: " << blue[r10] << setw(15);
-            hold2 = calculate(blue[r10], counterblue, blueav);
-            blueav = hold2;
             counterblue = counterblue + 1;
-            cout << blueav << " Blue Average " << endl;
+            blueav = calculate(blue[r10], counterblue, blueav);
+            counterall = counterall + 1;
+            allav = calculate(blue[r10], counterall, allav);
             break;
 
         case 3:
-            cout << i << " current red array pick: " << red[r10] << setw(15);
-            hold3 = calculate(red[r10], counterred, redav);
-            redav = hold3;
             counterred = counterred + 1;
-            cout << redav << " Red Average " << endl;
+            redav = calculate(red[r10], counterred, redav);
+            counterall = counterall + 1;
+            allav = calculate(red[r10], counterall, allav);
             break;
         }
         greenv.push_back(greenav);
         bluev.push_back(blueav);
         redv.push_back(redav);
+        allv.push_back(allav);
     }
 
-    int gs = greenv.size();
-    int bs = bluev.size();
-    int rs = redv.size();
-    /*
-    ofstream greenf("../green.csv");
-    for (int i1 = 0; i1 < gs; i1++) {
-        greenf << "green" << "," << greenv.at(i1) << "," << endl;
-    }
-    greenf.close();
-
-    ofstream bluef("../blue.csv");
-    for (int i2 = 0; i2 < bs; i2++) {
-        bluef << "blue" << "," << bluev.at(i2) << "," << endl;
-    }
-    bluef.close();
-
-
-    ofstream redf("../red.csv");
-    for (int i3 = 0; i3 < rs; i3++) {
-        redf << "red" << "," << redv.at(i3) << "," << endl;
-    }
-    redf.close();
-    */
     ofstream allf("../all.csv");
-    allf << "green," << "blue," << "red" << endl;
+    allf << "green," << "blue," << "red," << "all average" << endl;
     for (int i = 0; i < 100; i++) {
-        allf << greenv.at(i) << "," << bluev.at(i) << "," << redv.at(i) << endl;
+        allf << greenv.at(i) << "," << bluev.at(i) << "," << redv.at(i) << "," << allv.at(i) << endl;
     }
     allf.close();
 
@@ -102,12 +104,13 @@ int main() {
     cout << "GREEN AVERAGE: " << greenav << endl;
     cout << "BLUE AVERAGE: " << blueav << endl;
     cout << "RED AVERAGE: " << redav << endl;
+    cout << "ALL AVERAGE: " << allav << endl;
     return 0;
 }
 
 
 //this function finds the new average
-//x is new value to add, y is the average for array, i is the count
+//next is new value to add, average is the average for array, counter is the count
 double calculate(double next, double counter, double average) {
     double avg = average + ((1 / counter) * (next - average));
     return avg;
@@ -120,11 +123,10 @@ int start() {
     return r;
 }
 
-
+//this function tells us if we should explore or exploit
 int evaluate(double aa, double bb, double cc) {
     int chance = rand() % 100 + 1;
-
-    if (chance > 15) {
+    if (chance > 0) {
         if ((aa == bb) && (bb == cc)) {//case where all are equal
             int x = rand() % 3 + 1;
             return x;
@@ -132,6 +134,16 @@ int evaluate(double aa, double bb, double cc) {
         else if ((aa == bb) && (aa && bb > cc)) {//case where green is same as blue but greater than red
             int y = rand() % 2 + 1;
             return y;
+        }
+        else if ((aa == cc) && (aa && cc > bb)) {
+            int notbb[2] = { 1,3 };
+            int z = rand() % 2;
+            return notbb[z];
+        }
+        else if ((bb == cc) && (bb && cc > aa)) {
+            int notaa[2] = { 2,3 };
+            int u = rand() % 2;
+            return notaa[u];
         }
         else if (aa > bb && aa > cc) {//aa is largest
             return 1;
@@ -145,20 +157,15 @@ int evaluate(double aa, double bb, double cc) {
     }
     else
     {
-        //cout << "\nWE OUT HERE EXPLORING FOLKS! \n" << endl;
-        int y = rand() % 2;
-        int largest = evaluate(aa, bb, cc);
-        if (largest == 1) {
-            int notgreen[2] = { 2,3 };
-            return notgreen[y];
+        int y = rand() % 3 + 1;
+        if (y == 1) {
+            return 1;
         }
-        else if (largest == 2) {
-            int notblue[2] = { 1,3 };
-            return notblue[y];
+        else if (y == 2) {
+            return 2;
         }
-        else if (largest == 3) {
-            int notred[2] = { 1,2 };
-            return notred[y];
+        else if (y == 3) {
+            return 3;
         }
     }
 }
